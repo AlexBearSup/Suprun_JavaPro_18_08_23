9af090ab3af31600c7ac10f67c235071b9f820a3
AlexBearSup <138615943+AlexBearSup@users.noreply.github.com>
2023-09-20 17:03:13 +0300

    Delete src/main/java/homeworks directory

--- src/main/java/homeworks/homework4/participants_and_obstacles/participants/robot.java
+++
@@ -1,7 +0,0 @@
-package homeworks.homework4.participants_and_obstacles.participants;
-
-public class robot extends Participant {
-    public robot(String name, int powerRun, double powerJump) {
-        super(name, powerRun, powerJump);
-    }
-}
