a86126a89d6ce7596bcea8bef9413bdd6f7a1b7a
Suprun Oleksandr <1specktr1@gmail.com>
2023-09-27 12:26:14 +0300

    add homework 8.1

---
+++ src/main/java/homeworks/homework7_1/Order.java
@@ -0,0 +1,35 @@
+package homeworks.homework7_1;
+
+public class Order {
+    private String  client;
+    private int numberOrder;
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "client='" + client + '\'' +
+                ", numberOrder=" + numberOrder +
+                '}';
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+    public void setNumberOrder(int numberOrder) {
+        this.numberOrder = numberOrder;
+    }
+
+    public String getClient() {
+        return client;
+    }
+
+    public int getNumberOrder() {
+        return numberOrder;
+    }
+
+    public Order(String client, int numberOrder) {
+        this.client = client;
+        this.numberOrder = numberOrder;
+    }
+}
